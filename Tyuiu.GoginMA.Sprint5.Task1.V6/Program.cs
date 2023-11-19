ad6b4cc06c187629cb31066816c3a4f10e2b94a8
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-11-19 18:28:41 +0500

    Task1

---
+++ Tyuiu.GoginMA.Sprint5.Task1.V6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GoginMA.Sprint5.Task1.V6.Lib;
+
+namespace Tyuiu.GoginMA.Sprint5.Task1.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #5                                                              *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #6                                                             *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дана функция, произвести табулирование f(x) на заданном диапазоне      *");
+            Console.WriteLine("* [-5; 5] с шагом 1. Результат сохранить в текстовый файл и вывести на   *");
+            Console.WriteLine("* консоль в таблицу.                                                     *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
