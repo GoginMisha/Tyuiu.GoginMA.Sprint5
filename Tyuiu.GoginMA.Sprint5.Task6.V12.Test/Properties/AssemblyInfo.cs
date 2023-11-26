8a68c38c8f65205a1f0ee0097c0df4df0bb81828
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-11-26 13:44:05 +0500

    Task6

---
+++ Tyuiu.GoginMA.Sprint5.Task6.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoginMA.Sprint5.Task6.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoginMA.Sprint5.Task6.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4aa6f02-b988-4077-b157-b4326ab3087c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GoginMA.Sprint5.Task6.V12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GoginMA.Sprint5.Task6.V12.Lib;
+
+namespace Tyuiu.GoginMA.Sprint5.Task6.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #5                                                              *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                       *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #12                                                            *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан файл, в котором есть набор символьных данных. Найти количество     *");
+            Console.WriteLine("* удвоенных букв нн в заданной строке.                                   *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask6V12.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
